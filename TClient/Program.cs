142d9711f2462b97dfd912b4abf2e00cf89ed170
tiaoyu <tiaoyu@tiaoyus-mac.lan>
2019-05-27 00:38:07 +0800

    Create projs.

---
+++ TClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Client!");
+        }
+    }
+}
---
+++ TServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Server!");
+        }
+    }
+}
