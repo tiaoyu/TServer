c45b79489e917c404e6d98804c44830523624f4b
tiaoyu <tiaoyu@tiaoyus-mac.lan>
2019-05-28 00:22:34 +0800

    add a simple socket.

--- TClient/Program.cs
+++ TClient/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using TClient.Simple;
 
 namespace TClient
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Client!");
+
+            SimpleSocketClient client = new SimpleSocketClient("127.0.0.1", 11000);
+            client.Connect();
         }
+
+
     }
 }
