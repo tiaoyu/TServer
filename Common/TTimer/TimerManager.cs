f0b51c683cf2b32ec2909e5f3c030bf2ea02bc3e
tiaoyu <txydsx@live.com>
2019-11-19 18:45:56 +0800

    update timermanager:add a timer;update tick;

--- Common/TTimer/TimerManager.cs
+++ Common/TTimer/TimerManager.cs
@@ -10,12 +10,17 @@ namespace Common.TTimer
     /// </summary>
     public class Timer
     {
-        Timer next;
+        // 定时器待执行方法
         public Action<object> callback;
+        public object UserData;
+        // 下一次触发的时间
         public long NextTriggerTick;
+        // 需要触发的次数
+        public int TriggerCount;
+        // 两次触发的时间间隔
+        public int TriggerInterval;
         public Timer()
         {
-            next = null;
         }
     }
 
@@ -24,16 +29,34 @@ namespace Common.TTimer
     /// </summary>
     public class Wheel
     {
+        // 总刻度
         public int slotCount;
+
+        // 当前指向刻度
         public int curSlot;
+
         // 每个刻度对应一个链表
-        public Timer[] head;
+        public LinkedList<Timer>[] head;
 
         public Wheel(int slotCount)
         {
             this.slotCount = slotCount;
             curSlot = 0;
-            head = new Timer[slotCount];
+            head = new LinkedList<Timer>[slotCount];
+            for (var i = 0; i < slotCount; ++i)
+            {
+                head[i] = new LinkedList<Timer>();
+            }
+        }
+
+        /// <summary>
+        /// 添加定时器
+        /// </summary>
+        /// <param name="timer"></param>
+        /// <param name="slot"></param>
+        public void Add(Timer timer, int slot)
+        {
+            head[slot].AddLast(timer);
         }
     }
 
@@ -45,17 +68,25 @@ namespace Common.TTimer
         private const int MAX_WHEEL_COUNT = 5; // 时间轮个数
         private const int SLOT_TIME_MS = 10;   // 时间轮精度 10ms
         private int[] WheelSlotCount = new int[] { 1 << 8, 1 << 4, 1 << 4, 1 << 4, 1 << 4 };
+        // 所有轮
         public Wheel[] Wheels;
         // 当前时间点 ms
-        public long CurrentTick; 
+        public long CurrentTick;
+        // 缓存的Timer
+        public List<Timer> tempTimers;
 
+        /// <summary>
+        /// 初始化
+        /// </summary>
         public void Init()
         {
+            tempTimers = new List<Timer>();
             Wheels = new Wheel[MAX_WHEEL_COUNT];
             for (var i = 0; i < MAX_WHEEL_COUNT; ++i)
             {
                 Wheels[i] = new Wheel(WheelSlotCount[i]);
             }
+            Update(10);
         }
 
         /// <summary>
@@ -64,15 +95,46 @@ namespace Common.TTimer
         /// </summary>
         public void Insert(int delay, int interval, int count, object userData, Action<object> callback)
         {
-#warning TODO
             // 构建定时器
             var t = new Timer();
             t.callback = callback;
             t.NextTriggerTick = CurrentTick + delay;
-            // 计算插进时间轮中的位置
-            var pos = delay / SLOT_TIME_MS;
+            t.TriggerCount = count;
+            t.TriggerInterval = interval;
+
+            Insert(t);
+        }
+
+        /// <summary>
+        /// 插入定时器
+        /// 定义1轮的单位时间为10ms，即一格10ms，设当前1轮指针所指在刻度 curSlot，当前时间为 CurrentTicks，要插入的定时器到期时间为 NextTriggerTick，时间统一按照时间戳表示，
+        ///    则插入的位置算法为：
+        /// 在1轮中的指针位置（这里的减一是因为起始数为0而不是1，0~255正好256格）
+        /// pos = ((NextTriggerTick - CurrentTick) / 10  - 1 + curSlot) % 256  
+        /// 若pos 大于 256 ，则将pos带入2轮进行计算，类似进位
+        /// 循环：2 pos = (pos -1 + curSlot) % 64 
+        /// </summary>
+        /// <param name="timer"></param>
+        private void Insert(Timer timer)
+        {
+            // 计算当前到下次触发时间所占的总刻度数
+            var pos = 0L;
+            // 每个轮挨个计算最终插入的位置
             for (var i = 0; i < MAX_WHEEL_COUNT; ++i)
             {
+                if (i == 0)
+                    pos = (Math.Max((timer.NextTriggerTick - CurrentTick) / SLOT_TIME_MS - 1, 0) + Wheels[0].curSlot);
+                else
+                    pos = (Math.Max(pos - 1, 0) + Wheels[i].curSlot);
+
+                if (pos >= WheelSlotCount[i])
+                {
+                    pos = pos / WheelSlotCount[i];
+                    continue;
+                }
+
+                Wheels[i].Add(timer, (int)pos);
+                break;
             }
         }
 
@@ -80,9 +142,56 @@ namespace Common.TTimer
         /// 更新定时器
         /// 每帧更新一次
         /// </summary>
-        public void Update(int time)
+        public void Update(int futureTick)
         {
-            
+            //if (CurrentTick == 0)
+            //{
+            //    CurrentTick = futureTick;
+            //    return;
+            //}
+            for (; CurrentTick < futureTick; CurrentTick += SLOT_TIME_MS)
+            {
+                // 更新
+                for (var i = 0; i < MAX_WHEEL_COUNT; ++i)
+                {
+                    var curSlot = Wheels[i].curSlot;
+                    Wheels[i].curSlot++;
+                    // 执行当前Slot上的 timer
+                    while (Wheels[i].head[curSlot].Count != 0)
+                    {
+                        var timer = Wheels[i].head[curSlot].First.Value;
+                        Wheels[i].head[curSlot].RemoveFirst();
+
+                        // 只有在最大轮上的timer才是真正需要执行的timer
+                        if (i == 0)
+                        {
+                            timer.callback(timer.UserData);
+                            --timer.TriggerCount;
+                            timer.NextTriggerTick = CurrentTick + timer.TriggerInterval;
+                        }
+
+                        if (timer.TriggerCount > 0)
+                        {
+                            tempTimers.Add(timer);
+                        }
+                    }
+
+                    foreach (var timer in tempTimers)
+                    {
+                        Insert(timer);
+                    }
+                    tempTimers.Clear();
+
+
+                    Wheels[i].curSlot %= WheelSlotCount[i];
+
+                    if (Wheels[i].curSlot == 0)
+                    {
+                        continue;
+                    }
+                    break;
+                }
+            }
         }
     }
 }
---
+++ TServerTests/TTimer/TimerManagerTests.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+using Common.TTimer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace Common.TTimer.Tests
+{
+    public class TimerManagerTests
+    {
+        private readonly ITestOutputHelper output;
+        private TimerManager tmgr;
+        public TimerManagerTests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.tmgr = new TimerManager();
+            tmgr.Init();
+            tmgr.Insert(10, 10, 1, null, (obj) =>
+            {
+                output.WriteLine($"{tmgr.CurrentTick}: hi!");
+            });
+            tmgr.Insert(20, 10, 1, null, (obj) =>
+            {
+                output.WriteLine($"{tmgr.CurrentTick}: hi!");
+            });
+            tmgr.Insert(30, 10, 1, null, (obj) =>
+            {
+                output.WriteLine($"{tmgr.CurrentTick}: hi!");
+            });
+        }
+
+        [Fact()]
+        public void InsertTest()
+        {
+            tmgr.Insert(40, 0, 1, null, (obj) =>
+            {
+                output.WriteLine("hi!");
+            });
+            Assert.True(true);
+        }
+
+        [Fact()]
+        public void UpdateTest()
+        {
+            tmgr.Update(1000);
+            tmgr.Insert(0, 10, 10, null, (obj) =>
+            {
+                output.WriteLine($"{tmgr.CurrentTick}: one_hi!");
+            });
+            tmgr.Insert(10, 10, 10, null, (obj) =>
+            {
+                output.WriteLine($"{tmgr.CurrentTick}: two_hi!");
+            });
+            tmgr.Insert(20, 10, 10, null, (obj) =>
+            {
+                output.WriteLine($"{tmgr.CurrentTick}: three_hi!");
+            });
+            tmgr.Insert(3000, 3000, 10, null, (obj) =>
+            {
+                output.WriteLine($"{tmgr.CurrentTick}: four_hi!");
+            });
+            tmgr.Update(300000);
+            Assert.True(true);
+        }
+    }
+}
