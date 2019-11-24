142cfc40fa2c6074db253351a11aaa0016a51aed
tiaoyu <txydsx@live.com>
2019-11-24 23:41:39 +0800

    add Redis utils -- CSRedis

---
+++ TServer/Cache/RedisCache.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Common.Cache
+{
+    public class RedisCache
+    {
+        public RedisCache()
+        {
+            var csredis = new CSRedis.CSRedisClient("127.0.0.1:6379,defaultDatabase=13,prefix=T_");
+            RedisHelper.Initialization(csredis);
+        }
+
+        public static void Set(string key, byte[] value)
+        {
+            RedisHelper.Set(key, value);
+        }
+
+        public static byte[] Get(string key)
+        {
+            return RedisHelper.Get<byte[]>(key);
+        }
+    }
+}
