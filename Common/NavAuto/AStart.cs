e4109169fa7f6d42015c70e77fec0ca9cf1f3d5b
tiaoyu <txydsx@live.com>
2020-03-02 19:06:07 +0800

    准备添加寻路逻辑

---
+++ Common/NavAuto/AStart.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.NavAuto
+{
+    public class AStart
+    {
+        public void SetSourcePos(MapData map, int x, int y) { }
+
+        public void SetTargetPos(MapData map, int x, int y) { }
+
+        public int GValue(MapData map, int x, int y) { return 0; }
+
+        public int HValue(MapData map, int x, int y) { return 0; }
+
+        public int FValue(MapData map, int x, int y) { return 0; }
+
+        public void CalOptimalPath(MapData map, int sX, int sY, int tX, int tY)
+        {
+            // 定义OpenList CloseList
+            var openList = new List<Node>();
+            var closeList = new List<Node>();
+            openList.Add(new Node { x = sX, y = sY });
+
+
+            // Step 1 从OpenList中挑选出F值最小的Node 称之为 CurNode 对其进行广度搜索深度为1 获得 ExpNodeList
+            
+            // Step 2 若 CurNode 就是目标点 则算法结束 进入End 阶段
+
+            // Step 3.1 将 CurNode 从OpenList中移除并加入CloseList中 
+            // Step 3.2 遍历ExpNodeList, 若 ExpNode 已存在于OpenList中 则判断 FValue(CurNode) + FValue(ExpNode,CurNode) < FValue(ExpNode)
+
+            // Step Generate Optimal Path 回溯生成最短路径
+        }
+    }
+
+    public class Node
+    {
+        public int GValue;
+        public int HValue;
+        public int FValue;
+        public int x;
+        public int y;
+        public Node Parent;
+    }
+}
---
+++ Common/NavAuto/MapData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Common.NavAuto
+{
+    public class MapData
+    {
+        private readonly string filePath;
+        private int length;
+        private int width;
+
+        public int[,] map;
+
+        public int Width { get => width; set => width = value; }
+        public int Length { get => length; set => length = value; }
+
+        public MapData(string path)
+        {
+            filePath = path;
+        }
+
+        public bool Init()
+        {
+            var f = File.ReadAllLines(filePath);
+            var lineCount = f.Length;
+
+            if (f.Length <= 1) return false;
+
+            // 第一行为地图长宽 逗号分隔
+            var size = f[0].Split(',');
+
+            if (size.Length < 2) return false;
+            if (!int.TryParse(size[0], out var l)) return false;
+            if (!int.TryParse(size[1], out var w)) return false;
+
+            Length = l;
+            Width = w;
+            map = new int[l, w];
+
+            for (var i = 1; i < lineCount; ++i)
+            {
+                var row = f[i].ToCharArray();
+                for (var j = 0; j < row.Length; ++i)
+                {
+                    if (row[j] >= 48 && row[j] <= 57)
+                        map[i - 1, j] = row[j] - 48;
+                    else return false;
+                }
+            }
+            return true;
+        }
+    }
+}
